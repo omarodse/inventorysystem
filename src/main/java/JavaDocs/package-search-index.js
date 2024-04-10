cc2f389a201f568a627711c1ed477d372c8cc35e
omarodse <omarcarpi@gmail.com>
2024-04-10 08:20:06 -0400

    Modify JavaDocs folder

---
+++ src/main/java/JavaDocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"wgu.inventoryfxmlapp","l":"controller"},{"m":"wgu.inventoryfxmlapp","l":"model"},{"m":"wgu.inventoryfxmlapp","l":"wgu.inventoryfxmlapp"}];updateSearchResults();
